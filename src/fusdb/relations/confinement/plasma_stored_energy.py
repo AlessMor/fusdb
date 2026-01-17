05aac78190ee534e72a4007f73be0f8d6286f0db
AlessMor <alessandro.morandi@polito.it>
2026-01-17 17:56:28 +0100

    added definitions for p_th (alias p_tot_avg), p_peak

--- src/fusdb/relations/confinement/plasma_stored_energy.py
+++ src/fusdb/relations/confinement/plasma_stored_energy.py
@@ -2,15 +2,8 @@
 
 from __future__ import annotations
 
-from fusdb.registry.constants import KEV_TO_J
 from fusdb.reactor_class import Reactor
 from fusdb.relation_util import nonzero, symbol
-
-
-@Reactor.relation("plasma", name="Thermal pressure", output="p_th")
-def thermal_pressure(n_e: float, T_e: float, n_i: float, T_i: float) -> float:
-    """Return thermal pressure from electron/ion densities and temperatures."""
-    return (n_e * T_e + n_i * T_i) * KEV_TO_J
 
 
 @Reactor.relation("plasma", name="Thermal stored energy", output="W_th", variables=("p_th", "V_p"))
@@ -31,6 +24,5 @@ def energy_confinement_time(W_th: float, P_loss: float) -> float:
     return W_th / P_loss
 
 
-THERMAL_PRESSURE_REL = thermal_pressure.relation
 THERMAL_ENERGY_REL = thermal_stored_energy.relation
 CONFINEMENT_REL = energy_confinement_time.relation
--- src/fusdb/relations/plasma_pressure/plasma_pressure.py
+++ src/fusdb/relations/plasma_pressure/plasma_pressure.py
@@ -1,5 +1,17 @@
-# TODO(high): from cfspopcon add
-    # p_tot_avg
-    # p_peak
-    
-#TODO(high): move p_th here from plasma_stored_energy.py
+"""Plasma pressure relations expressed via Reactor.relation."""
+from __future__ import annotations
+
+from fusdb.registry.constants import KEV_TO_J
+from fusdb.reactor_class import Reactor
+
+
+@Reactor.relation("plasma", name="Thermal pressure", output="p_th")
+def thermal_pressure(n_e: float, T_e: float, n_i: float, T_i: float) -> float:
+    """Return thermal pressure from electron/ion densities and temperatures."""
+    return (n_e * T_e + n_i * T_i) * KEV_TO_J
+
+
+@Reactor.relation("plasma", name="Peak pressure", output="p_peak")
+def peak_pressure(n0: float, T0: float, n_i_peak: float, T_i_peak: float) -> float:
+    """Calculate the peak pressure."""
+    return (n0 * T0 + n_i_peak * T_i_peak) * KEV_TO_J
