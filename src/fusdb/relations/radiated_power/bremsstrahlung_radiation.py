6031ef8106f68f6b0be7c6f112cfe1c2fa53de83
Alex <alessandro.morandi@polito.it>
2026-06-15 15:09:39 -0400

    fixed error in bremsstrahlung relaiton

--- src/fusdb/relations/radiated_power/bremsstrahlung_radiation.py
+++ src/fusdb/relations/radiated_power/bremsstrahlung_radiation.py
@@ -31,5 +31,5 @@ def bremsstrahlung_radiation(n_e: float, T_e: float, Z_eff: float, V_p: float, r
     n_e20 = n_e / 1e20
     Tm = 511.0  # keV, electron rest mass energy
     xrel = (1.0 + 2.0 * T_e / Tm) * (1.0 + (2.0 / Z_eff) * (1.0 - 1.0 / (1.0 + T_e / Tm)))
-    p_brem = 5.35e-3 * Z_eff * (n_e20 ** 2) * (T_e ** 0.5) * xrel / 1e6  # [W/m^3]
+    p_brem = 5.35e-3 * Z_eff * (n_e20 ** 2) * (T_e ** 0.5) * xrel * 1e6  # [W/m^3]
     return V_p * trapezoid(p_brem, x=rho)
